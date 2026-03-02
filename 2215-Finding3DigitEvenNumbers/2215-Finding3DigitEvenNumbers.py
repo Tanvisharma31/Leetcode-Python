da8b13762d9ab1729ee2c99c65994593dad69451
Tanvi  Sharma <2004tanvisharma@gmail.com>
2026-03-02 13:59:25 +0530

    Create 2215-Finding3DigitEvenNumbers

---
+++ 2215-Finding3DigitEvenNumbers/2215-Finding3DigitEvenNumbers.py
@@ -0,0 +1,17 @@
+# Last updated: 02/03/2026, 13:59:13
+class Solution:
+  def findEvenNumbers(self, digits: list[int]) -> list[int]:
+    ans = []
+    count = collections.Counter(digits)
+
+    # Try to construct `abc`.
+    for a in range(1, 10):
+      for b in range(0, 10):
+        for c in range(0, 9, 2):
+          if count[a] > 0 and count[b] > (
+                  b == a) and count[c] > (
+                  c == a) + (
+                  c == b):
+            ans.append(a * 100 + b * 10 + c)
+
+    return ans
