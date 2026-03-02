64ced924c711377fbb4708c1dd93ce803490020c
Tanvi  Sharma <2004tanvisharma@gmail.com>
2026-03-02 14:01:03 +0530

    Create 1293-ThreeConsecutiveOdds

---
+++ 1293-ThreeConsecutiveOdds/1293-ThreeConsecutiveOdds.py
@@ -0,0 +1,9 @@
+# Last updated: 02/03/2026, 14:00:52
+class Solution:
+  def threeConsecutiveOdds(self, arr: list[int]) -> bool:
+    count = 0
+    for a in arr:
+      count = 0 if a % 2 == 0 else count + 1
+      if count == 3:
+        return True
+    return False
