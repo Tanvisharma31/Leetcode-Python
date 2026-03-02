7da47f788b7bdd5698d23f5d6f4fe41902ea6b27
Tanvi  Sharma <2004tanvisharma@gmail.com>
2026-03-02 13:59:22 +0530

    Create 2249-CountTheHiddenSequences

---
+++ 2249-CountTheHiddenSequences/2249-CountTheHiddenSequences.py
@@ -0,0 +1,10 @@
+# Last updated: 02/03/2026, 13:59:11
+class Solution:
+  def numberOfArrays(
+      self,
+      differences: list[int],
+      lower: int,
+      upper: int,
+  ) -> int:
+    prefix = list(itertools.accumulate(differences, initial=0))
+    return max(0, (upper - lower) - (max(prefix) - min(prefix)) + 1)
