5781b97129f2d236f3b79b50d15e899d9272d5b4
Tanvi  Sharma <2004tanvisharma@gmail.com>
2026-03-02 14:00:53 +0530

    Create 1402-CountSquareSubmatricesWithAllOnes

---
+++ 1402-CountSquareSubmatricesWithAllOnes/1402-CountSquareSubmatricesWithAllOnes.py
@@ -0,0 +1,31 @@
+# Last updated: 02/03/2026, 14:00:42
+class Solution:
+    def countSquares(self, matrix: List[List[int]]) -> int:
+        r = len(matrix)
+        c = len(matrix[0])
+        dp=[[0]*c for _ in range(r)]
+
+        count=0
+
+        for j in range(c):
+            if(matrix[0][j]==1):
+                dp[0][j]=1
+                count+=1
+        
+        for i in range(1, r):
+            if(matrix[i][0]==1):
+                dp[i][0]=1
+                count+=1
+
+        for i in range(1, r):
+            for j in range(1, c):
+                if(matrix[i][j]==0):
+                    continue
+                elif dp[i-1][j]==0 or dp[i-1][j-1]==0 or dp[i][j-1]==0:
+                    dp[i][j]=1
+                else:
+                    dp[i][j] = min(dp[i-1][j], dp[i-1][j-1], dp[i][j-1]) + 1
+                
+                count+=dp[i][j]
+        
+        return count
