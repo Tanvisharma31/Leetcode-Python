edbd5e4a712ac83556c4f1b5ee6f1b0ff4d28fac
Tanvi  Sharma <2004tanvisharma@gmail.com>
2026-03-02 13:57:08 +0530

    Create 3445-LexicographicallyMinimumStringAfterRemovingStars

---
+++ 3445-LexicographicallyMinimumStringAfterRemovingStars/3445-LexicographicallyMinimumStringAfterRemovingStars.py
@@ -0,0 +1,24 @@
+# Last updated: 02/03/2026, 13:56:57
+class Solution:
+    def clearStars(self, s: str) -> str:
+        if "*" not in s:
+            return s
+        h = []
+        stacks = [[] for _ in range(26)]
+        for i, c in enumerate(s):
+            if c == "*":
+                if not h:
+                    continue
+                x = h[0]
+                if not (v := stacks[x]):
+                    continue
+                v.pop()
+                if not v:
+                    heapq.heappop(h)
+            else:
+                x = ord(c) - 97
+                v = stacks[x]
+                if not v:
+                    heapq.heappush(h, x)
+                v.append(i)
+        return "".join(s[i] for i in sorted(itertools.chain.from_iterable(stacks)))
