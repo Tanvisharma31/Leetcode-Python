baf69a54641ed4153f2484d506c826ad7dcb60af
Tanvi  Sharma <2004tanvisharma@gmail.com>
2026-03-02 13:57:09 +0530

    Create 3430-CountDaysWithoutMeetings

---
+++ 3430-CountDaysWithoutMeetings/3430-CountDaysWithoutMeetings.py
@@ -0,0 +1,13 @@
+# Last updated: 02/03/2026, 13:56:58
+class Solution:
+    def countDays(self, days: int, meetings: List[List[int]]) -> int:
+        meetings.sort()
+        prevEnd = 0
+
+        for start, end in meetings:
+            start = max(start, prevEnd + 1)
+            length = end - start + 1
+            days -= max(length, 0)
+            prevEnd = max(prevEnd, end)
+
+        return days
