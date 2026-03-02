254c0b4149217dd760123808018119e1f0adb07a
Tanvi  Sharma <2004tanvisharma@gmail.com>
2026-03-02 14:03:15 +0530

    Create 70-ClimbingStairs

---
+++ 70-ClimbingStairs/70-ClimbingStairs.py
@@ -0,0 +1,12 @@
+# Last updated: 02/03/2026, 14:03:04
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n<=3: return n
+        prev1=3
+        prev2=2
+        cur=0
+        for _ in range(3,n):
+            cur=prev1+prev2
+            prev2=prev1
+            prev1=cur
+        return cur
