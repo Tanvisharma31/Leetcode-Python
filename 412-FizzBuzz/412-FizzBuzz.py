8f9a1d20363e38f38097802321d9eaeb492c844f
Tanvi  Sharma <2004tanvisharma@gmail.com>
2026-03-02 14:02:28 +0530

    Create 412-FizzBuzz

---
+++ 412-FizzBuzz/412-FizzBuzz.py
@@ -0,0 +1,27 @@
+# Last updated: 02/03/2026, 14:02:17
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        res = []
+        i, fizz, buzz = 1, 0, 0
+
+        while i <= n:
+            fizz += 1
+            buzz += 1
+
+            if fizz == 3 and buzz == 5:
+                res.append("FizzBuzz")
+                fizz = buzz = 0
+            elif fizz == 3:
+                res.append("Fizz")
+                fizz = 0
+            elif buzz == 5:
+                res.append("Buzz")
+                buzz = 0
+            else:
+                res.append(str(i))
+
+            i += 1
+
+        return res
+
+        
