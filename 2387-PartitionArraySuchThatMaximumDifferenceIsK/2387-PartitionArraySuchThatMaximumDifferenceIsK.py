bbff176bf6706a9238d1215d22c849d52fc960a6
Tanvi  Sharma <2004tanvisharma@gmail.com>
2026-03-02 13:59:10 +0530

    Create 2387-PartitionArraySuchThatMaximumDifferenceIsK

---
+++ 2387-PartitionArraySuchThatMaximumDifferenceIsK/2387-PartitionArraySuchThatMaximumDifferenceIsK.py
@@ -0,0 +1,18 @@
+# Last updated: 02/03/2026, 13:58:58
+class Solution:
+    def partitionArray(self, nums: List[int], k: int) -> int:
+        if len(nums)==1: return 1
+        xMax=max(nums)
+        freq=[0]*(1+xMax)
+        for x in nums:
+            freq[x]+=1
+        par, x=0, 0
+        while x<=xMax:
+            while x<=xMax and freq[x]==0:
+                x+=1
+            if x>xMax: break
+            end=x+k
+            par+=1
+            x=end+1
+        return par
+        
