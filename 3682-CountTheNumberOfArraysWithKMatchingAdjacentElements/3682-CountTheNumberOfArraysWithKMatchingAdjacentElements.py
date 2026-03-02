96b789f1501f45e2b56df926e12e779d5174a34a
Tanvi  Sharma <2004tanvisharma@gmail.com>
2026-03-02 13:56:38 +0530

    Create 3682-CountTheNumberOfArraysWithKMatchingAdjacentElements

---
+++ 3682-CountTheNumberOfArraysWithKMatchingAdjacentElements/3682-CountTheNumberOfArraysWithKMatchingAdjacentElements.py
@@ -0,0 +1,19 @@
+# Last updated: 02/03/2026, 13:56:27
+
+mx = 10**5 + 10
+mod = 10**9 + 7
+f = [1] + [0] * mx
+g = [1] + [0] * mx
+
+for i in range(1, mx):
+    f[i] = f[i - 1] * i % mod
+    g[i] = pow(f[i], mod - 2, mod)
+
+
+def comb(m: int, n: int) -> int:
+    return f[m] * g[n] * g[m - n] % mod
+
+
+class Solution:
+    def countGoodArrays(self, n: int, m: int, k: int) -> int:
+        return comb(n - 1, k) * m * pow(m - 1, n - k - 1, mod) % mod
