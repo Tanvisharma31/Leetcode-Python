d5c95d79f1c722865477eab5147c5211be4e29b9
Tanvi  Sharma <2004tanvisharma@gmail.com>
2026-03-02 14:01:58 +0530

    Create 745-FindSmallestLetterGreaterThanTarget

---
+++ 745-FindSmallestLetterGreaterThanTarget/745-FindSmallestLetterGreaterThanTarget.py
@@ -0,0 +1,27 @@
+# Last updated: 02/03/2026, 14:01:47
+class Solution:
+    def nextGreatestLetter(self, letters: List[str], target: str) -> str:
+        # n = len(letters)
+        # for i in range(n):
+        #     if ord(letters[i]) > ord(target) :
+        #         return letters[i]
+        # else:
+        #     return letters[0]
+        n = len(letters)
+        low = 0; high = n-1
+        min1 = "z" 
+        count = 0
+        while low<=high:
+            mid = (low+high)//2
+            # if ord(letters[mid]) > ord(target):
+            if letters[mid] > target:
+                min1 = min(letters[mid],min1)
+                high = mid - 1
+                count = 1
+            else:
+                low = mid + 1
+        if count == 0:
+            return letters[0]
+        else:
+            return min1
+        
