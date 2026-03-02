9c9d9570fb13df290dbb4476ede950d0da353c91
Tanvi  Sharma <2004tanvisharma@gmail.com>
2026-03-02 14:01:23 +0530

    Create 1002-MaximumWidthRamp

---
+++ 1002-MaximumWidthRamp/1002-MaximumWidthRamp.cpp
@@ -0,0 +1,18 @@
+// Last updated: 02/03/2026, 14:01:12
+class Solution {
+ public:
+  int maxWidthRamp(vector<int>& nums) {
+    int ans = 0;
+    stack<int> stack;
+
+    for (int i = 0; i < nums.size(); ++i)
+      if (stack.empty() || nums[i] < nums[stack.top()])
+        stack.push(i);
+
+    for (int i = nums.size() - 1; i > ans; --i)
+      while (!stack.empty() && nums[i] >= nums[stack.top()])
+        ans = max(ans, i - stack.top()), stack.pop();
+
+    return ans;
+  }
+};
