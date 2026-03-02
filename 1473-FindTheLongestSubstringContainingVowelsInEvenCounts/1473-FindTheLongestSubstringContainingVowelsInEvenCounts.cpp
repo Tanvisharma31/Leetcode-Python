998c6b3035e437572a3fad39afe39a3911dd1b56
Tanvi  Sharma <2004tanvisharma@gmail.com>
2026-03-02 14:00:42 +0530

    Create 1473-FindTheLongestSubstringContainingVowelsInEvenCounts

---
+++ 1473-FindTheLongestSubstringContainingVowelsInEvenCounts/1473-FindTheLongestSubstringContainingVowelsInEvenCounts.cpp
@@ -0,0 +1,21 @@
+// Last updated: 02/03/2026, 14:00:31
+class Solution {
+ public:
+  int findTheLongestSubstring(string s) {
+    constexpr string_view kVowels = "aeiou";
+    int ans = 0;
+    int prefix = 0;  // the binary prefix
+    unordered_map<int, int> prefixToIndex{{0, -1}};
+
+    for (int i = 0; i < s.length(); ++i) {
+      const int index = kVowels.find(s[i]);
+      if (index != -1)
+        prefix ^= 1 << index;
+      if (!prefixToIndex.contains(prefix))
+        prefixToIndex[prefix] = i;
+      ans = max(ans, i - prefixToIndex[prefix]);
+    }
+
+    return ans;
+  }
+};
