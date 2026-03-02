afbf6d5e5925a3b903795491946f193a5f195de5
Tanvi  Sharma <2004tanvisharma@gmail.com>
2026-03-02 13:56:24 +0530

    Create 3773-MinimumPairRemovalToSortArrayI

---
+++ 3773-MinimumPairRemovalToSortArrayI/3773-MinimumPairRemovalToSortArrayI.py
@@ -0,0 +1,25 @@
+# Last updated: 02/03/2026, 13:56:13
+class Solution:
+    def minimumPairRemoval(self, nums: List[int]) -> int:
+        c=0
+        i=0
+        n=len(nums)
+        while i<n-1:
+            if(nums[i]>nums[i+1]):
+                mini=math.inf
+                index=0
+                for j in range(n-1):
+                    if(mini>(nums[j]+nums[j+1])):
+                        mini=nums[j]+nums[j+1]
+                        index=j
+                nums[index]=mini
+                nums.pop(index+1)
+                c=c+1
+                n=n-1
+                i=0
+            else:
+                i=i+1
+        return c
+
+
+        
