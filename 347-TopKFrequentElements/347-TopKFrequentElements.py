b6d55e1fa0cf7e80def2468ed1774aab0ea53972
Tanvi  Sharma <2004tanvisharma@gmail.com>
2026-03-02 14:02:33 +0530

    Create 347-TopKFrequentElements

---
+++ 347-TopKFrequentElements/347-TopKFrequentElements.py
@@ -0,0 +1,7 @@
+# Last updated: 02/03/2026, 14:02:22
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        d=defaultdict(int)
+        for n in nums:
+            d[n]+=1
+        return sorted(d,key=d.get,reverse=True)[:k]
