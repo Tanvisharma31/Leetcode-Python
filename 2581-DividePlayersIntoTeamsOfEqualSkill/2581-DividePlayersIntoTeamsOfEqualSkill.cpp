1d938b5b5545654d1461ae0863555e4d0fd3158e
Tanvi  Sharma <2004tanvisharma@gmail.com>
2026-03-02 13:58:50 +0530

    Create 2581-DividePlayersIntoTeamsOfEqualSkill

---
+++ 2581-DividePlayersIntoTeamsOfEqualSkill/2581-DividePlayersIntoTeamsOfEqualSkill.cpp
@@ -0,0 +1,23 @@
+// Last updated: 02/03/2026, 13:58:39
+class Solution {
+ public:
+  long long dividePlayers(vector<int>& skill) {
+    const int n = skill.size();
+    const int teamSkill = accumulate(skill.begin(), skill.end(), 0) / (n / 2);
+    long ans = 0;
+    unordered_map<int, int> count;
+
+    for (const int s : skill)
+      ++count[s];
+
+    for (const auto& [s, freq] : count) {
+      const int requiredSkill = teamSkill - s;
+      if (const auto it = count.find(requiredSkill);
+          it == count.cend() || it->second != freq)
+        return -1;
+      ans += static_cast<long>(s) * requiredSkill * freq;
+    }
+
+    return ans / 2;
+  }
+};
