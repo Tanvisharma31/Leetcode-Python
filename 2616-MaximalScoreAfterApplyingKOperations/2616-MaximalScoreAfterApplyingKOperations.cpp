037032963296aeddc79de4b4f796ed06d2880cb4
Tanvi  Sharma <2004tanvisharma@gmail.com>
2026-03-02 13:58:46 +0530

    Create 2616-MaximalScoreAfterApplyingKOperations

---
+++ 2616-MaximalScoreAfterApplyingKOperations/2616-MaximalScoreAfterApplyingKOperations.cpp
@@ -0,0 +1,20 @@
+// Last updated: 02/03/2026, 13:58:35
+class Solution {
+ public:
+  long long maxKelements(vector<int>& nums, int k) {
+    long ans = 0;
+    priority_queue<int> maxHeap;
+
+    for (const int num : nums)
+      maxHeap.push(num);
+
+    for (int i = 0; i < k; ++i) {
+      const int num = maxHeap.top();
+      maxHeap.pop();
+      ans += num;
+      maxHeap.push((num + 2) / 3);
+    }
+
+    return ans;
+  }
+};
