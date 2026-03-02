4b8fd1d581e2b8c665d80304ab5d891f2f2ed935
Tanvi  Sharma <2004tanvisharma@gmail.com>
2026-03-02 13:59:52 +0530

    Create 2050-CountGoodNumbers

---
+++ 2050-CountGoodNumbers/2050-CountGoodNumbers.py
@@ -0,0 +1,14 @@
+# Last updated: 02/03/2026, 13:59:41
+class Solution:
+    def countGoodNumbers(self, n: int) -> int:    
+
+        MOD = 10 ** 9 + 7
+        
+        def expo(x: int, num: int) -> int:
+            if num == 0:
+                return 1  
+            elif num % 2 == 0:
+                return expo(x ** 2 % MOD, num // 2)
+            return x * expo(x, num - 1) % MOD
+
+        return 5 ** (n % 2) * expo(20, n // 2) % MOD
