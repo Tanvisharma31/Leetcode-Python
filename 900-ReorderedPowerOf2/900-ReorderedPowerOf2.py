06da520f3227f91de2451c8282d9e979af391db4
Tanvi  Sharma <2004tanvisharma@gmail.com>
2026-03-02 14:01:34 +0530

    Create 900-ReorderedPowerOf2

---
+++ 900-ReorderedPowerOf2/900-ReorderedPowerOf2.py
@@ -0,0 +1,13 @@
+# Last updated: 02/03/2026, 14:01:23
+class Solution:
+    def reorderedPowerOf2(self, n: int) -> bool:
+        def count_digits(x):
+            return ''.join(sorted(str(x)))
+
+        target = count_digits(n)
+        
+        # Precompute all powers of 2 within range
+        for i in range(31):  # 2^0 to 2^30
+            if count_digits(1 << i) == target:
+                return True
+        return False
