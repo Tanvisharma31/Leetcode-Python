4eb56eff9c338f64d387bd7f11d5247fd91a45d4
Tanvi  Sharma <2004tanvisharma@gmail.com>
2026-03-02 13:59:50 +0530

    Create 2089-MaximumMatrixSum

---
+++ 2089-MaximumMatrixSum/2089-MaximumMatrixSum.cpp
@@ -0,0 +1,21 @@
+// Last updated: 02/03/2026, 13:59:38
+class Solution {
+ public:
+  long long maxMatrixSum(vector<vector<int>>& matrix) {
+    long absSum = 0;
+    int minAbs = INT_MAX;
+    // 0 := even number of negatives
+    // 1 := odd number of negatives
+    int oddNeg = 0;
+
+    for (const vector<int>& row : matrix)
+      for (const int num : row) {
+        absSum += abs(num);
+        minAbs = min(minAbs, abs(num));
+        if (num < 0)
+          oddNeg ^= 1;
+      }
+
+    return absSum - oddNeg * minAbs * 2;
+  }
+};
