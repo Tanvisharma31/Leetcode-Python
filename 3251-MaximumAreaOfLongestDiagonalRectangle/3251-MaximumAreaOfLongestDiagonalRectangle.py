b9cb6681ae3e458a8ec18568993aac4745263022
Tanvi  Sharma <2004tanvisharma@gmail.com>
2026-03-02 13:57:27 +0530

    Create 3251-MaximumAreaOfLongestDiagonalRectangle

---
+++ 3251-MaximumAreaOfLongestDiagonalRectangle/3251-MaximumAreaOfLongestDiagonalRectangle.py
@@ -0,0 +1,5 @@
+# Last updated: 02/03/2026, 13:57:16
+class Solution:
+  def areaOfMaxDiagonal(self, dimensions: list[list[int]]) -> int:
+    a, b = max(dimensions, key=lambda x: (x[0]**2 + x[1]**2, x[0] * x[1]))
+    return a * b
