d1538e2917cc13521cc25533215438e1280d4b87
Tanvi  Sharma <2004tanvisharma@gmail.com>
2026-03-02 13:59:36 +0530

    Create 2144-MaximumDifferenceBetweenIncreasingElements

---
+++ 2144-MaximumDifferenceBetweenIncreasingElements/2144-MaximumDifferenceBetweenIncreasingElements.py
@@ -0,0 +1,12 @@
+# Last updated: 02/03/2026, 13:59:25
+class Solution:
+  def maximumDifference(self, nums: list[int]) -> int:
+    ans = -1
+    mn = nums[0]
+
+    for i in range(len(nums)):
+      if nums[i] > mn:
+        ans = max(ans, nums[i] - mn)
+      mn = min(mn, nums[i])
+
+    return ans
