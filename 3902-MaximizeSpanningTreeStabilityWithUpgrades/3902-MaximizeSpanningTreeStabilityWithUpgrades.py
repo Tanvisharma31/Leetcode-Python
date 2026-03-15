f26b54f01e5bc363629991a22da604f3addfb4d1
Tanvi  Sharma <2004tanvisharma@gmail.com>
2026-03-15 19:00:45 +0530

    Create 3902-MaximizeSpanningTreeStabilityWithUpgrades

---
+++ 3902-MaximizeSpanningTreeStabilityWithUpgrades/3902-MaximizeSpanningTreeStabilityWithUpgrades.py
@@ -0,0 +1,71 @@
+# Last updated: 15/03/2026, 19:00:37
+class Solution:
+    def maxStability(self, n: int, edges: List[List[int]], k: int) -> int:
+        
+        parents = list(range(n))
+
+        def find(i):            
+            i2 = i
+            while parents[i] != i:
+                i = parents[i]
+            if parents[i2] != i:
+                parents[i2] = i
+            return i
+
+        def union(p1, p2):
+            parents[p2] = p1
+
+        count = n
+
+        heap = []
+        min_w = inf
+        for s,t,w,m in edges:
+            if m:
+                if w < min_w:
+                    min_w = w
+                p1 = find(s)
+                p2 = find(t)
+                if p1 != p2:
+                    count -= 1
+                    union(p1,p2)
+                else:
+                    return -1
+            else:
+                heapq.heappush(heap,(-w, s, t))
+
+        if count == 1:
+            return min_w
+
+        #print(min_w)
+
+        heap2 = []
+        while heap:
+            w, s, t = heapq.heappop(heap)
+
+            p1 = find(s)
+            p2 = find(t)
+            if p1 == p2:
+                continue
+            else:
+                count -= 1
+                union(p1,p2)
+
+            #print(w,s,t, heap2, k)
+            if k > 0:
+                heapq.heappush(heap2, w)
+                k -= 1
+                w = -w * 2
+            else:
+                w = min(-w * 2, - heappushpop(heap2, w))
+            #print(w)
+            if w < min_w:
+                min_w = w
+
+            if count == 1:
+                return min_w
+
+        return -1
+
+
+            
+            
