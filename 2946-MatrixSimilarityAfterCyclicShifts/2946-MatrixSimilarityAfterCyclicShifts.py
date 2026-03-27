2ee15113b292ddcec9787b4ebe2046be47d48be2
Tanvi  Sharma <2004tanvisharma@gmail.com>
2026-03-27 10:15:39 +0530

    File updated at 27/03/2026, 10:15:32

--- 2946-MatrixSimilarityAfterCyclicShifts/2946-MatrixSimilarityAfterCyclicShifts.py
+++ 2946-MatrixSimilarityAfterCyclicShifts/2946-MatrixSimilarityAfterCyclicShifts.py
@@ -1,19 +1,13 @@
-# Last updated: 27/03/2026, 10:15:09
-1class Solution:
-2    def areSimilar(self, mat, k):
-3        m, n = len(mat), len(mat[0])
-4        
-5        k %= n  # (reduce k<n)
-6        
-7        for i in range(m):
-8            for j in range(n):
-9                if i % 2 == 0:
-10                    # even row , left shift
-11                    if mat[i][j] != mat[i][(j + k) % n]:
-12                        return False
-13                else:
-14                    # odd row , right shift
-15                    if mat[i][j] != mat[i][(j - k) % n]:
-16                        return False
-17        
-18        return True
+# Last updated: 27/03/2026, 10:15:32
+class Solution:
+    def areSimilar(self, mat: List[List[int]], k: int) -> bool:
+        n = len(mat)
+        m = len(mat[0])
+        k %= m
+        if not k:
+            return True
+        for i in range(n):
+            row = mat[i][(-1) ** (i) * k:] + mat[i][:(-1) ** (i) * k]
+            if mat[i] != row:
+                return False
+        return True
