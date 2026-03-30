5497636cc3d7491a09525995890f8ec1ebd45ecb
Tanvi  Sharma <2004tanvisharma@gmail.com>
2026-03-30 10:34:32 +0530

    File updated at 30/03/2026, 10:34:31

--- 2840-CheckifStringsCanbeMadeEqualWithOperationsII/2840-CheckifStringsCanbeMadeEqualWithOperationsII.py
+++ 2840-CheckifStringsCanbeMadeEqualWithOperationsII/2840-CheckifStringsCanbeMadeEqualWithOperationsII.py
@@ -1,18 +1,9 @@
-# Last updated: 30/03/2026, 10:34:14
-1class Solution:
-2    def checkStrings(self, s1: str, s2: str) -> bool:
-3        prime = [
-4            2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41,
-5            43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101
-6        ]
-7
-8        mod = 10**9 + 7
-9        h1 = [1, 1]
-10        h2 = [1, 1]
-11
-12        for i in range(len(s1)):
-13            off = i & 1
-14            h1[off] = (h1[off] * prime[ord(s1[i]) - ord('a')]) % mod
-15            h2[off] = (h2[off] * prime[ord(s2[i]) - ord('a')]) % mod
-16
-17        return h1 == h2
+# Last updated: 30/03/2026, 10:34:31
+class Solution:
+    def checkStrings(self, s1: str, s2: str) -> bool:
+        # strategy:
+        # 1. odd indexes should contain the same elements in s1 and s2        
+        # 2. even indexes should contain the same elements in s1 and s2
+
+        return Counter(s1[::2]) == Counter(s2[::2]) and \
+               Counter(s1[1::2]) == Counter(s2[1::2])
