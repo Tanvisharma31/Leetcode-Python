b0af9eb4b19c9dc6eb457f7bf4e7a8d8631c714a
Tanvi  Sharma <2004tanvisharma@gmail.com>
2026-04-05 15:36:07 +0530

    File updated at 05/04/2026, 15:36:02

--- 657-RobotReturntoOrigin/657-RobotReturntoOrigin.py
+++ 657-RobotReturntoOrigin/657-RobotReturntoOrigin.py
@@ -1,19 +1,4 @@
-# Last updated: 05/04/2026, 15:35:39
-1class Solution:
-2    def judgeCircle(self, moves: str) -> bool:
-3        if len(moves) & 1: return False
-4        x, y = [0, 0]
-5
-6        move = {
-7            'U': (0, 1),
-8            'D': (0, -1),
-9            'L': (-1, 0),
-10            'R': (1, 0),
-11        }
-12
-13        for c in moves:
-14            dx, dy = move[c]
-15            x += dx
-16            y += dy
-17
-18        return [x, y] == [0, 0]
+# Last updated: 05/04/2026, 15:36:02
+class Solution:
+    def judgeCircle(self, moves: str) -> bool:
+        return moves.count('U')==moves.count('D') and moves.count('L')==moves.count('R')
