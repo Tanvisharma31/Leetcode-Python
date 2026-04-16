6972ae99edde00e64c1a64aa61ecc01d61a13143
Tanvi  Sharma <2004tanvisharma@gmail.com>
2026-04-16 15:29:45 +0530

    File updated at 16/04/2026, 15:29:25

--- 3488-ClosestEqualElementQueries/3488-ClosestEqualElementQueries.py
+++ 3488-ClosestEqualElementQueries/3488-ClosestEqualElementQueries.py
@@ -1,36 +1,35 @@
-# Last updated: 16/04/2026, 15:29:08
-1class Solution:
-2    def solveQueries(self, nums: List[int], queries: List[int]) -> List[int]:
-3        n = len(nums)
-4        mp = defaultdict(list)
-5
-6        # store indices
-7        for i in range(n):
-8            mp[nums[i]].append(i)
-9
-10        ans = []
-11
-12        for q in queries:
-13            v = mp[nums[q]]
-14
-15            # only one time present
-16            if len(v) == 1:
-17                ans.append(-1)
-18                continue
-19
-20            pos = bisect_left(v, q)
-21            res = float('inf')
-22
-23            # left neighbor
-24            left = v[(pos - 1) % len(v)]
-25            d1 = abs(q - left)
-26            res = min(res, min(d1, n - d1))
-27
-28            # right neighbor
-29            right = v[(pos + 1) % len(v)]
-30            d2 = abs(q - right)
-31            res = min(res, min(d2, n - d2))
-32
-33            ans.append(res)
-34
-35        return ans
+# Last updated: 16/04/2026, 15:29:25
+class Solution:
+    def solveQueries(self, nums: List[int], queries: List[int]) -> List[int]:
+        N = len(nums)
+        ans = []
+        first = {}
+        record = {}
+
+        for i in range(N):
+            if nums[i] not in record:
+                ans.append(inf)
+                record[nums[i]] = i
+                first[nums[i]] = i
+
+            else:
+                dist = i - record[nums[i]]
+                ans.append(dist)
+                ans[record[nums[i]]] = min(ans[record[nums[i]]], dist)
+                record[nums[i]] = i
+
+            
+        # Explicit circular case.
+        for num in first:
+            if first[num] != record[num]:
+                dist = N - record[num] + first[num]
+                ans[record[num]] = min(ans[record[num]], dist)
+                ans[first[num]] = min(ans[first[num]], dist)
+
+        res = []
+        for index in queries:
+            if ans[index] == inf:
+                ans[index] = -1
+            res.append(ans[index])
+
+        return res
