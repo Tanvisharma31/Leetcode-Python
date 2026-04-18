8255bed05cb72277a80460312d2a9a95b278865b
Tanvi  Sharma <2004tanvisharma@gmail.com>
2026-04-18 16:18:46 +0530

    File updated at 18/04/2026, 16:18:24

--- 3783-MirrorDistanceofanInteger/3783-MirrorDistanceofanInteger.py
+++ 3783-MirrorDistanceofanInteger/3783-MirrorDistanceofanInteger.py
@@ -1,9 +1,6 @@
-# Last updated: 18/04/2026, 16:18:07
-1class Solution:
-2    def mirrorDistance(self, n: int) -> int:
-3        rev, x=0, n
-4        while x>0:
-5            x, r=divmod(x, 10)
-6            rev=10*rev+r
-7        return abs(rev-n)
-8        
+# Last updated: 18/04/2026, 16:18:24
+class Solution:
+    def mirrorDistance(self, n: int) -> int:
+
+        return abs(n - int(str(n)[::-1]))
+        
