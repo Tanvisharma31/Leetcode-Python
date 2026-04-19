a5787aee64e4e53ea86c3fb2df83959a840e522d
Tanvi  Sharma <2004tanvisharma@gmail.com>
2026-04-19 17:38:59 +0530

    File updated at 19/04/2026, 17:38:35

--- 1855-MaximumDistanceBetweenaPairofValues/1855-MaximumDistanceBetweenaPairofValues.py
+++ 1855-MaximumDistanceBetweenaPairofValues/1855-MaximumDistanceBetweenaPairofValues.py
@@ -1,10 +1,12 @@
-# Last updated: 19/04/2026, 17:38:05
-1class Solution:
-2    def maxDistance(self, A: List[int], B: List[int]) -> int:
-3        i, j = 0, 1
-4
-5        while i < len(A) and j < len(B):
-6            i += A[i] > B[j]
-7            j += 1
-8
-9        return j - i - 1
+# Last updated: 19/04/2026, 17:38:35
+class Solution:
+    def maxDistance(self, nums1: List[int], nums2: List[int]) -> int:
+        m, n = len(nums1), len(nums2)
+        i = 0
+        for j in range(n):
+            if nums1[i] > nums2[j]:
+                i += 1
+                if i >= m:
+                    break
+
+        return max(j - i, 0)
