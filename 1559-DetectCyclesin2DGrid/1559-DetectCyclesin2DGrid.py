bb02a4d6a80f35b0037141bc7416c1a3a0b0c321
Tanvi  Sharma <2004tanvisharma@gmail.com>
2026-04-26 13:05:07 +0530

    File updated at 26/04/2026, 13:05:01

--- 1559-DetectCyclesin2DGrid/1559-DetectCyclesin2DGrid.py
+++ 1559-DetectCyclesin2DGrid/1559-DetectCyclesin2DGrid.py
@@ -1,22 +1,28 @@
-# Last updated: 26/04/2026, 13:04:35
-1class Solution:
-2    def containsCycle(self, grid: List[List[str]]) -> bool:
-3        m, n = len(grid), len(grid[0])
-4        visit = [False] * (m * n)
-5        dirs = ((0, -1), (0, 1), (-1, 0), (1, 0))
-6
-7        def dfs(r, c, pr, pc):
-8            visit[r * n + c] = True
-9
-10            for dr, dc in dirs:
-11                nr, nc = r + dr, c + dc
-12
-13                if (nr, nc) != (pr, pc):
-14                    if 0 <= nr < m and 0 <= nc < n:
-15                        if grid[nr][nc] == grid[r][c]:
-16                            if visit[nr * n + nc] or dfs(nr, nc, r, c):
-17                                return True
-18
-19            return False
-20
-21        return any(not visit[i] and dfs(i // n, i % n, -1, -1) for i in range(m * n))
+# Last updated: 26/04/2026, 13:05:01
+class Solution:
+    def containsCycle(self, grid: List[List[str]]) -> bool:
+        m, n = len(grid), len(grid[0])
+        parent = list(range(m * n))
+
+        def find(x):
+            while parent[x] != x:
+                parent[x] = parent[parent[x]]
+                x = parent[x]
+            return x
+
+        def union(a, b):
+            ra, rb = find(a), find(b)
+            if ra == rb:
+                return True
+            parent[ra] = rb
+            return False
+
+        for i in range(m):
+            for j in range(n):
+                if j + 1 < n and grid[i][j] == grid[i][j+1]:
+                    if union(i*n+j, i*n+j+1):
+                        return True
+                if i + 1 < m and grid[i][j] == grid[i+1][j]:
+                    if union(i*n+j, (i+1)*n+j):
+                        return True
+        return False
