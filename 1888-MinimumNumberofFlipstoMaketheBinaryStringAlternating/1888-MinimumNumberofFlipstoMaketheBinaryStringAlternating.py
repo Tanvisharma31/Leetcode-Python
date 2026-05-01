6b868c40375356eb35c685c6078487366a7bf65e
Tanvi  Sharma <2004tanvisharma@gmail.com>
2026-05-01 11:53:31 +0530

    Create 1888-MinimumNumberofFlipstoMaketheBinaryStringAlternating

---
+++ 1888-MinimumNumberofFlipstoMaketheBinaryStringAlternating/1888-MinimumNumberofFlipstoMaketheBinaryStringAlternating.py
@@ -0,0 +1,18 @@
+# Last updated: 01/05/2026, 11:53:18
+1class Solution:
+2    def minFlips(self, s: str) -> int:
+3        n = len(s)
+4        res = n
+5        op = [0, 0]
+6
+7        for i in range(n):
+8            op[(ord(s[i]) ^ i) & 1] += 1
+9
+10        for i in range(n):
+11            c = ord(s[i])
+12            op[(c ^ i) & 1] -= 1
+13            op[(c ^ (n + i)) & 1] += 1
+14            res = min(res, min(op))
+15
+16        return res
+17
