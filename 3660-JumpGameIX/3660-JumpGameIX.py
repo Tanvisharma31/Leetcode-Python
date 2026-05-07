195a929af440d9a1fc634796717f947cbb57ea42
Tanvi  Sharma <2004tanvisharma@gmail.com>
2026-05-07 12:49:26 +0530

    File updated at 5/7/2026, 12:49:16 PM

--- 3660-JumpGameIX/3660-JumpGameIX.py
+++ 3660-JumpGameIX/3660-JumpGameIX.py
@@ -1,25 +1,37 @@
-# Last updated: 5/7/2026, 12:48:57 PM
-1class Solution:
-2    def maxValue(self, nums: List[int]) -> List[int]:
-3        n = len(nums)
-4        if n == 0: return []
-5
-6        suffix_min = [0] * n
-7        suffix_min[n - 1] = nums[n - 1]
-8        for i in range(n - 2, -1, -1):
-9            suffix_min[i] = min(suffix_min[i + 1], nums[i])
-10
-11        res = [0] * n
-12        prefix_max = float('-inf')
-13        chunk_start = 0
-14
-15        for i in range(n):
-16            prefix_max = max(prefix_max, nums[i])
-17
-18            if i == n - 1 or prefix_max <= suffix_min[i + 1]:
-19                for j in range(chunk_start, i + 1):
-20                    res[j] = prefix_max
-21                chunk_start = i + 1
-22                prefix_max = float('-inf')
-23
-24        return res
+# Last updated: 5/7/2026, 12:49:16 PM
+class Solution:
+    def maxValue(self, nums: List[int]) -> List[int]:
+        N = len(nums)
+        ans = [nums[0]] # prefix
+        for i in range(1, N):
+            ans.append(max(ans[-1], nums[i]))
+
+        min_idx = N - 1
+        for i in range(N - 2, -1, -1):
+            if ans[i] > nums[min_idx]:
+                ans[i] = ans[min_idx]
+            if nums[i] < nums[min_idx]:
+                min_idx = i
+        return ans
+            
+
+'''
+2  3  1
+2, 3, 3
+
+2 1 | 8 6 | 7 3 2 2 | 11  7 | 14 13
+2 2   8 8   8 8 8 8   11 11   14 14
+                      7   7   13 13 
+
+              0, 6
+              2, 8
+              7, 11
+
+'''
+
+
+
+
+
+
+        
