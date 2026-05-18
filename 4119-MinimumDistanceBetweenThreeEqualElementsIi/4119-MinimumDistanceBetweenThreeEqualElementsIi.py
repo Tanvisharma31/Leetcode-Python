9da97d3a72455bec8662d9c60942a834766e44e1
Tanvi  Sharma <2004tanvisharma@gmail.com>
2026-05-18 17:00:49 +0530

    Create 4119-MinimumDistanceBetweenThreeEqualElementsIi

---
+++ 4119-MinimumDistanceBetweenThreeEqualElementsIi/4119-MinimumDistanceBetweenThreeEqualElementsIi.py
@@ -0,0 +1,18 @@
+# Last updated: 5/18/2026, 5:00:42 PM
+class Solution:
+    def minimumDistance(self, nums: List[int]) -> int:
+        min_length = len(nums) + 1
+
+        last_indices = [-1] * min_length
+        second_to_last_indices = [-1] * min_length
+
+        min_dist = math.inf
+        for i, num in enumerate(nums):
+            if second_to_last_indices[num] != -1:
+                dist = i - second_to_last_indices[num]
+                if min_dist > dist:
+                    min_dist = dist
+            second_to_last_indices[num], last_indices[num] = last_indices[num], i
+        if min_dist == math.inf:
+            return -1
+        return 2 * min_dist
