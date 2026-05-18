352bad2f72459d313cbd0d5722175b0dc1bd454b
Tanvi  Sharma <2004tanvisharma@gmail.com>
2026-05-18 13:05:12 +0530

    File updated at 5/18/2026, 1:05:04 PM

--- 1345-JumpGameIV/1345-JumpGameIV.py
+++ 1345-JumpGameIV/1345-JumpGameIV.py
@@ -1,29 +1,59 @@
-# Last updated: 5/18/2026, 1:04:44 PM
-1class Solution:
-2    def minJumps(self, arr: List[int]) -> int:
-3        n=len(arr)
-4        vis=[False]*n
-5        mp=defaultdict(list)
-6        for i, x in enumerate(arr):
-7            mp[x].append(i)
-8        q=deque([0])
-9        step=0
-10        while q:
-11            s=len(q)
-12            for _ in range(s):
-13                cur=q.popleft()
-14                if cur==n-1: return step
-15                if cur>=1 and not vis[cur-1]:
-16                    q.append(cur-1)
-17                    vis[cur-1]=True
-18                if cur<=n and not vis[cur+1]:
-19                    q.append(cur+1)
-20                    vis[cur+1]=True
-21                x=arr[cur]
-22                for idx in mp[x]:
-23                    if not vis[idx]:
-24                        q.append(idx)
-25                        vis[idx]=True
-26                mp[x].clear()
-27            step+=1
-28        return -1
+# Last updated: 5/18/2026, 1:05:04 PM
+class Solution:
+    '''
+    Approach 1:
+        * DFS - return min(dfs(i+1), dfs(i-1), for j in sameVal[i]: dfs(j))
+    Approach 2:
+        * MultiSource BFS from start
+    Approach 3:
+        * Bidirectional multisource bfs - from start and end explore the path with lesser nodes in the layer
+    '''
+    def minJumps(self, arr: List[int]) -> int:
+        n = len(arr)
+        if n <= 1:
+            return 0
+        valueToIdx = {}
+        for i, num in enumerate(arr):
+            if num in valueToIdx:
+                valueToIdx[num].append(i)
+            else:
+                valueToIdx[num] = [i]
+
+        visited = {0, n-1}
+        jumps = 0
+        start = set([0])
+        end = set([n-1])
+        while start:
+            if len(start) > len(end):
+                start, end = end, start
+            nextQ = set()
+            for i in start:
+                             
+                for j in valueToIdx[arr[i]]:
+                    if j in end:
+                        return jumps+1
+                    
+                    if j not in visited:
+                        nextQ.add(j)
+                        visited.add(j)
+                # clear list to prevent redundant search
+                valueToIdx[arr[i]].clear()
+
+                if i-1 in end or i+1 in end:
+                    return jumps+1
+                
+                if i-1 >= 0 and i-1 not in visited:
+                    nextQ.add(i-1)
+                    visited.add(i-1)
+    
+                if i+1 < n and i+1 not in visited:
+                    nextQ.add(i+1)
+                    visited.add(i+1)
+            jumps+=1
+            start = nextQ
+        return -1
+
+
+
+        
+            
