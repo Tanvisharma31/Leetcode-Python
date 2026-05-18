8048ee5be16924ec0fde8f08f6421d05e0bcba56
Tanvi  Sharma <2004tanvisharma@gmail.com>
2026-05-18 17:01:02 +0530

    Create 3981-JumpGameIx

---
+++ 3981-JumpGameIx/3981-JumpGameIx.py
@@ -0,0 +1,37 @@
+# Last updated: 5/18/2026, 5:00:54 PM
+class Solution:
+    def maxValue(self, nums: List[int]) -> List[int]:
+        N = len(nums)
+        ans = [nums[0]] # prefix
+        for i in range(1, N):
+            ans.append(max(ans[-1], nums[i]))
+
+        min_idx = N - 1
+        for i in range(N - 2, -1, -1):
+            if ans[i] > nums[min_idx]:
+                ans[i] = ans[min_idx]
+            if nums[i] < nums[min_idx]:
+                min_idx = i
+        return ans
+            
+
+'''
+2  3  1
+2, 3, 3
+
+2 1 | 8 6 | 7 3 2 2 | 11  7 | 14 13
+2 2   8 8   8 8 8 8   11 11   14 14
+                      7   7   13 13 
+
+              0, 6
+              2, 8
+              7, 11
+
+'''
+
+
+
+
+
+
+        
