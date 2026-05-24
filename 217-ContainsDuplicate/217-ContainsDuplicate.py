3bc128f506d11f9a80c925bbb166048036dced28
Tanvi  Sharma <2004tanvisharma@gmail.com>
2026-05-24 19:38:39 +0530

    File updated at 5/24/2026, 7:38:21 PM

--- 217-ContainsDuplicate/217-ContainsDuplicate.py
+++ 217-ContainsDuplicate/217-ContainsDuplicate.py
@@ -1,6 +1,4 @@
-# Last updated: 5/24/2026, 7:37:52 PM
-class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        s = set(nums)
-
-        return  not(len(s) == len(nums))
+# Last updated: 5/24/2026, 7:38:21 PM
+1class Solution:
+2    def containsDuplicate(self, nums: List[int]) -> bool:
+3        return len(set(nums)) < len(nums)
