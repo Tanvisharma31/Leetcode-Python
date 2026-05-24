be351f7f0d631c5ad8dfb1b8e624e8535f17a22e
Tanvi  Sharma <2004tanvisharma@gmail.com>
2026-05-24 19:45:31 +0530

    File updated at 5/24/2026, 7:45:13 PM

--- 448-FindAllNumbersDisappearedinanArray/448-FindAllNumbersDisappearedinanArray.py
+++ 448-FindAllNumbersDisappearedinanArray/448-FindAllNumbersDisappearedinanArray.py
@@ -1,5 +1,43 @@
-# Last updated: 5/24/2026, 7:44:48 PM
-1class Solution:
-2    def findDisappearedNumbers(self, nums):
-3        nums.sort()
-4        return [i for i in range(1, len(nums)+1) if nums[bisect_left(nums, i)%len(nums)] != i]
+# Last updated: 5/24/2026, 7:45:13 PM
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        '''
+        counts=Counter(nums)
+        ans=[]
+
+        for i in range(1,len(nums)+1):
+            if i not in counts:
+                ans.append(i)
+        
+        return ans
+        
+        i=0
+        n=len(nums)
+
+        while i<n:
+            homeidx=nums[i]-1
+
+            if nums[i]!=nums[homeidx]:
+                nums[i],nums[homeidx]=nums[homeidx],nums[i]
+            
+            else:
+                i+=1
+        
+        ans=[]
+        for i in range(n):
+            if nums[i]!=i+1:
+                ans.append(i+1)
+        
+        return ans
+
+        '''
+        arr=[0]*(len(nums)+1)
+        ans=[]
+
+        for n in nums:
+            arr[n]=1
+        for i in range(1,len(nums)+1):
+            if arr[i]==0:
+                ans.append(i)
+        
+        return ans
