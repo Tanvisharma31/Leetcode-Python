2a1728ce43de2a15758d3998ff186d070bfdd1d0
Tanvi  Sharma <2004tanvisharma@gmail.com>
2026-05-26 11:33:35 +0530

    File updated at 5/26/2026, 11:33:14 AM

--- 3120-CounttheNumberofSpecialCharactersI/3120-CounttheNumberofSpecialCharactersI.py
+++ 3120-CounttheNumberofSpecialCharactersI/3120-CounttheNumberofSpecialCharactersI.py
@@ -1,19 +1,14 @@
-# Last updated: 5/26/2026, 11:32:49 AM
+# Last updated: 5/26/2026, 11:33:14 AM
 class Solution:
     def numberOfSpecialChars(self, word: str) -> int:
-        # a-z:97,122
-        # A-Z:65,90
-        res = [0]*26
-        seen_upper, seen_lower = set(), set()
-        for c in word:
-            if ord(c)>96 and c not in seen_lower:
-                res[97-ord(c)]+= 1
-                seen_lower.add(c)
-            if ord(c)<91 and c not in seen_upper:
-                res[65-ord(c)]+= 1
-                seen_upper.add(c)
-        ans = 0
-        for num in res:
-            if num==2:
-                ans+=1
-        return ans
+        seen = set()
+        counted = set()
+        count = 0
+
+        for letter in word:
+            other = letter.swapcase()
+            if other in seen and letter.lower() not in counted:
+                count += 1
+                counted.add(letter.lower())
+            seen.add(letter)
+        return count
