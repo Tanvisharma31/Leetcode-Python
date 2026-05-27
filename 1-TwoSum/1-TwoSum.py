f9fc4914c5edf60a235f937ed67f3fbb5a3c802d
Tanvi  Sharma <2004tanvisharma@gmail.com>
2026-05-27 19:12:59 +0530

    File updated at 5/27/2026, 7:12:36 PM

--- 1-TwoSum/1-TwoSum.py
+++ 1-TwoSum/1-TwoSum.py
@@ -1,12 +1,20 @@
-# Last updated: 5/27/2026, 7:12:15 PM
-1class Solution:
-2    def maxArea(self, height: List[int]) -> int:
-3        l, r=0, len(height)-1
-4        res=0
-5        while l<r:
-6            res=max(res,(r-l)* min(height[l],height[r]))
-7            if height[l]<height[r]:
-8                l+=1
-9            else:
-10                r-=1
-11        return res
+# Last updated: 5/27/2026, 7:12:36 PM
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        length=len(height)
+        left=0
+        right=length-1
+        area=(right-left)*min(height[left],height[right])
+        maxheight=max(height)
+        while left<right:
+            if height[left]<height[right]:
+                left+=1
+            else:
+                right-=1
+            area=max((right-left)*min(height[left],height[right]),area)
+            if area>=maxheight*(right-left):
+                return area
+        return area
+
+            
+            
