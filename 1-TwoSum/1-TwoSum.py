37becbd4cca77c9f72864b6311e96b6bc2e5cdbf
Tanvi  Sharma <2004tanvisharma@gmail.com>
2026-05-27 18:27:46 +0530

    File updated at 5/27/2026, 6:27:22 PM

--- 1-TwoSum/1-TwoSum.py
+++ 1-TwoSum/1-TwoSum.py
@@ -1,16 +1,15 @@
-# Last updated: 5/27/2026, 6:27:01 PM
-1class Solution:
-2    def twoSum(self, numbers: List[int], target: int) -> List[int]:
-3        l, r = 0, len(numbers) - 1
-4
-5        while l < r:
-6            s = numbers[l] + numbers[r]
-7
-8            if s == target:
-9                return [l + 1, r + 1]
-10
-11            elif s < target:
-12                l += 1
-13
-14            else:
-15                r -= 1
+# Last updated: 5/27/2026, 6:27:22 PM
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        ptr1 = 0
+        ptr2 = len(numbers)-1
+        while True:
+            total = numbers[ptr1]+numbers[ptr2]
+            if total>target:
+                ptr2-=1
+            elif total==target:
+                return [ptr1+1, ptr2+1]
+            else:
+                ptr1+=1
+
+        return -1
