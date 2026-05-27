83c4bb905ba5f7755d8f854f888b976fe380fdaa
Tanvi  Sharma <2004tanvisharma@gmail.com>
2026-05-27 18:27:24 +0530

    File updated at 5/27/2026, 6:27:01 PM

--- 1-TwoSum/1-TwoSum.py
+++ 1-TwoSum/1-TwoSum.py
@@ -1,9 +1,16 @@
-# Last updated: 5/24/2026, 7:42:04 PM
+# Last updated: 5/27/2026, 6:27:01 PM
 1class Solution:
-2    def twoSum(self, nums: List[int], target: int) -> List[int]:
-3        hashset={}
-4        for i, num in enumerate(nums):
-5            if target-num in hashset:
-6                return hashset[target-num],i
-7            hashset[num]=i
-8
+2    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+3        l, r = 0, len(numbers) - 1
+4
+5        while l < r:
+6            s = numbers[l] + numbers[r]
+7
+8            if s == target:
+9                return [l + 1, r + 1]
+10
+11            elif s < target:
+12                l += 1
+13
+14            else:
+15                r -= 1
