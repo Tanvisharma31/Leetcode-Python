1701e56b2a2279413d9dd091fae1a10cbaa1c3f9
Tanvi  Sharma <2004tanvisharma@gmail.com>
2026-05-30 08:47:45 +0530

    File updated at 5/30/2026, 8:47:17 AM

--- 242-ValidAnagram/242-ValidAnagram.py
+++ 242-ValidAnagram/242-ValidAnagram.py
@@ -1,11 +1,5 @@
-# Last updated: 5/30/2026, 8:46:59 AM
-1class Solution:
-2    def isAnagram(self, s: str, t: str) -> bool:
-3        if len(s)!=len(t):
-4            return False
-5        s=sorted(s)
-6        t=sorted(t)
-7        for c1,c2 in zip(s,t):
-8            if c1!=c2:
-9                return False
-10        return True
+# Last updated: 5/30/2026, 8:47:17 AM
+from collections import Counter
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return dict(Counter(s))==dict(Counter(t))
