9f76e1b29dae179d832b8694643f9352e73c7ccb
Tanvi  Sharma <2004tanvisharma@gmail.com>
2026-05-30 08:47:27 +0530

    File updated at 5/30/2026, 8:46:59 AM

--- 242-ValidAnagram/242-ValidAnagram.py
+++ 242-ValidAnagram/242-ValidAnagram.py
@@ -1,11 +1,11 @@
-# Last updated: 02/03/2026, 14:02:39
-class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        s1 = set(s + t)
-        valid = True
-        for i in s1:
-            if s.count(i) != t.count(i):
-                valid = False
-                break
-        return valid
-        
+# Last updated: 5/30/2026, 8:46:59 AM
+1class Solution:
+2    def isAnagram(self, s: str, t: str) -> bool:
+3        if len(s)!=len(t):
+4            return False
+5        s=sorted(s)
+6        t=sorted(t)
+7        for c1,c2 in zip(s,t):
+8            if c1!=c2:
+9                return False
+10        return True
