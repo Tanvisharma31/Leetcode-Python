77a330e38e3e6165ed21d5c524a8de35728670bd
Tanvi  Sharma <2004tanvisharma@gmail.com>
2026-05-30 13:36:41 +0530

    File updated at 5/30/2026, 1:36:13 PM

--- 219-ContainsDuplicateII/219-ContainsDuplicateII.py
+++ 219-ContainsDuplicateII/219-ContainsDuplicateII.py
@@ -1,19 +1,13 @@
-# Last updated: 5/30/2026, 1:35:59 PM
-1from typing import List
-2
-3class Solution:
-4    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-5        window_set = set()
-6        
-7        for i in range(len(nums)):
-8            # Remove element that is no longer in the window
-9            if i > k:
-10                window_set.remove(nums[i - k - 1])
-11            
-12            # Check for duplicate in the current window
-13            if nums[i] in window_set:
-14                return True
-15            
-16            window_set.add(nums[i])
-17            
-18        return False
+# Last updated: 5/30/2026, 1:36:13 PM
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        if len(nums)==len(set(nums)):
+            return False
+        else:
+            for i in range(len(nums)):
+                if nums[i] in nums[i+1:i+1+k]:
+                    return True
+            return False
+
+
+        
