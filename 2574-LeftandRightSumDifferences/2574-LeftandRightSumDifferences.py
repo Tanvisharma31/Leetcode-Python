c90a08dd2212bc582cceed8f77caaba38b4f7454
Tanvi  Sharma <2004tanvisharma@gmail.com>
2026-06-06 17:34:23 +0530

    File updated at 6/6/2026, 5:34:12 PM

--- 2574-LeftandRightSumDifferences/2574-LeftandRightSumDifferences.py
+++ 2574-LeftandRightSumDifferences/2574-LeftandRightSumDifferences.py
@@ -1,4 +1,11 @@
-# Last updated: 6/6/2026, 5:33:57 PM
-1class Solution:
-2    def leftRightDifference(self, nums: List[int]) -> List[int]:
-3        return (L:=list(accumulate(nums, initial=0))) and [abs(L[-1]-x-2*l) for l, x in zip(L, nums)]
+# Last updated: 6/6/2026, 5:34:12 PM
+class Solution:
+    def leftRightDifference(self, nums: List[int]) -> List[int]:
+        total_sum = sum(nums)
+        left_sum = 0
+        arr = []
+        for i,cur_val in enumerate(nums):
+            arr.append(abs(total_sum - left_sum - cur_val))
+            left_sum += cur_val
+            total_sum -= cur_val
+        return arr
