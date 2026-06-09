d42400f7c652c28b341698692c3e3535dab69669
Tanvi  Sharma <2004tanvisharma@gmail.com>
2026-06-09 09:30:09 +0530

    File updated at 6/9/2026, 9:29:52 AM

--- 3689-MaximumTotalSubarrayValueI/3689-MaximumTotalSubarrayValueI.py
+++ 3689-MaximumTotalSubarrayValueI/3689-MaximumTotalSubarrayValueI.py
@@ -1,4 +1,6 @@
-# Last updated: 6/9/2026, 9:29:33 AM
-1class Solution:
-2    def maxTotalValue(self, A: List[int], k: int) -> int:
-3        return k*max(A) - k*min(A)
+# Last updated: 6/9/2026, 9:29:52 AM
+class Solution:
+    def maxTotalValue(self, nums: List[int], k: int) -> int:
+        maxx = max(nums)
+        minn = min(nums)
+        return (maxx - minn) * k
