0e423e968d6d12aa3eddcc29ecc3d2362f724e61
Tanvi  Sharma <2004tanvisharma@gmail.com>
2026-06-28 17:05:14 +0530

    File updated at 6/28/2026, 5:04:49 PM

--- 1846-MaximumElementAfterDecreasingandRearranging/1846-MaximumElementAfterDecreasingandRearranging.py
+++ 1846-MaximumElementAfterDecreasingandRearranging/1846-MaximumElementAfterDecreasingandRearranging.py
@@ -1,11 +1,9 @@
-# Last updated: 6/28/2026, 5:04:33 PM
-1class Solution:
-2    def maximumElementAfterDecrementingAndRearranging(self, A: list[int]) -> int:
-3        A.sort()
-4        n = len(A)
-5
-6        A[0] = 1
-7        for i in range(1, n):
-8            A[i] = min(A[i], A[i - 1] + 1)
-9            
-10        return A[-1]
+# Last updated: 6/28/2026, 5:04:49 PM
+class Solution:
+    def maximumElementAfterDecrementingAndRearranging(self, arr):
+        arr.sort()
+        target = 1
+        for v in arr:
+            if v >= target:
+                target += 1
+        return target - 1
