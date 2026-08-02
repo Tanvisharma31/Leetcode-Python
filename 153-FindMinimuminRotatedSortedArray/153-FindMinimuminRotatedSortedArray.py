6c6d346afc858191f5b478c1ee4465ea11b97e1f
Tanvi  Sharma <2004tanvisharma@gmail.com>
2026-08-02 17:22:00 +0530

    File updated at 8/2/2026, 5:20:36 PM

--- 153-FindMinimuminRotatedSortedArray/153-FindMinimuminRotatedSortedArray.py
+++ 153-FindMinimuminRotatedSortedArray/153-FindMinimuminRotatedSortedArray.py
@@ -1,4 +1,4 @@
-# Last updated: 7/1/2026, 8:58:17 PM
+# Last updated: 8/2/2026, 5:20:36 PM
 1class Solution:
 2    def findMin(self, nums: List[int]) -> int:
 3        return nums[bisect_left(nums, True, key=lambda n: n <= nums[-1])]
